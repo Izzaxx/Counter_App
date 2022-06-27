3d654bb030ac2c1dbc22e214f1b416367ebc8ca5
Izzaxx <luisangelizaguirrehernandez@gmail.com>
2022-06-27 00:18:49 -0700

    [UPDATE] Center alig the text of counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 
 import 'package:flutter/material.dart';
 
+import 'package:counter_app/screens/home_screen.dart';
+
 void main() {
   runApp( const MyApp() );
 }
@@ -13,9 +15,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Center(
-        child: Text("Hello World"),
-      )
+      home: HomeScreen()
     );
   }
 
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,10 +4,25 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
 
   const HomeScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('HomeScreen'),
+        elevation: 10.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text("Clicks Counter"),
+            Text('10')
+          ],
+        ),
+      ),
+    );
   }
 
 }
